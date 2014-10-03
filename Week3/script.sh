2d09039d1693ec5dcf7a9643fcb496edab5c2218
X00093357 <marleysbr@gmail.com>
2014-10-03 15:43:44 +0100

    Added information

--- Week3/script.sh
+++ Week3/script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-#this is a script
-#this is a comment
+#: Title : script.sh
+#: Date : 03/10/2014
+#: Author : x00093357@ittd.ie
+#: Version : 1.0
+#: Description : CPU count script 
+#: Options :
 
 cpucnt=$( grep processor /proc/cpuinfo | wc -l )
 printf "The number of CPUs is: %s\n" $cpucnt
